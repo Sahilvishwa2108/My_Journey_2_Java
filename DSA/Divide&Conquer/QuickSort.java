2ccdd9daa946d55e16bbd5a712925e4eaff1d2e3
Sahil Vishwakarma <sahilvishwa2108@gmail.com>
2023-12-07 22:48:04 +0530

    added some more files

--- DSA/Divide&Conquer/QuickSort.java
+++ DSA/Divide&Conquer/QuickSort.java
@@ -8,15 +8,26 @@ public class QuickSort {
 
         //kaam
         int pivotIdx = partition(arr, si, ei);
+        sorting(arr, si, pivotIdx - 1); //left
+        sorting(arr, pivotIdx, ei);
     }
-    public static void partition(int arr[], int si, int ei){
+    public static int partition(int arr[], int si, int ei){
         int pivot = arr[ei];
         int i = si - 1;
 
-        for(int j = 0; j <= arr.length; j++){
-            i++;
-
+        for(int j = si; j < ei; j++){
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
         }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
     }
     public static void printArr(int arr[]){
         for(int i = 0; i < arr.length; i++){
@@ -26,5 +37,7 @@ public class QuickSort {
     }
     public static void main(String[] args) {
         int arr[] = {6,3,9,8,2,5};
+        sorting(arr, 0, 5);
+        printArr(arr);
     }
 }
